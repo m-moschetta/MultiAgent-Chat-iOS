7d19050582b805b3aa2646b83ede64c8e0758a7a
Mario Moschetta <63907937+m-moschetta@users.noreply.github.com>
2025-07-16 09:23:44 +0200

    Add main app entry point

---
+++ AgentChat/AgentChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgentChatApp.swift
+//  AgentChat
+//
+//  Created by Mario Moschetta on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct AgentChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ChatListView()
+        }
+    }
+}
